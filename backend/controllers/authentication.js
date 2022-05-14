38ff8e117631ab160210e53318e9a8a07dbb510b
Miguel(MgARL) <mgarlcr@gmail.com>
2022-05-14 11:01:50 -0400

    Added Sessions

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -14,7 +14,30 @@ router.post('/', async (req, res) => {
             message: `Could not find user with the provided username and password`
         })
     }else{
+        req.session.userId = user.userId
         res.status(200).json({ user })
+    }
+})
+
+router.get('/profile', async (req,res) => {
+    console.log(req.session.userId)
+    try {
+        let user = await User.findOne({
+            where:{ userId: req.session.userId}
+        })
+        res.json(user)
+    } catch (error) {
+        res.json(null)
+    }
+})
+
+router.post('/super-important-route', async (req, res) => {
+    if(req.session.userId){
+        console.log('Do the really super important thing')
+        res.send('done')
+    } else {
+        console.log('You are not authorized to do the super important thing')
+        res.send('Denied')
     }
 })
 
